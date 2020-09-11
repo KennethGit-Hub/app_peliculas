a0ec2924c4373adde326db454a008a4bf5ae0be8
KennethGit-Hub <kennethgio12.ktr@gmail.com>
2020-09-10 21:47:30 -0500

    cap99 corriendo apis

---
+++ lib/src/widgets/card_swiper_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class CardSwiper extends StatelessWidget {
+  final List<dynamic> peliculas;
+  CardSwiper({@required this.peliculas});
+
+  @override
+  Widget build(BuildContext context) {
+    final _screeSize = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.only(top: 24.0),
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: _screeSize.width * 0.7,
+        itemHeight: _screeSize.height * 0.5,
+        itemBuilder: (BuildContext context, int index) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.network(
+              "http://via.placeholder.com/350x150",
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        itemCount: peliculas.length,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
+      ),
+    );
+  }
+}
