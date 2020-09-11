3d0c1c464e916ba83c6ab725509b303fb19c52e7
KennethGit-Hub <kennethgio12.ktr@gmail.com>
2020-09-10 22:33:04 -0500

    capitulo 101 completo y corriendo sin errores

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+import 'package:app_peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+  String _apikey = 'e9b5346384e97565f9276c739c1a6bca';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-Es';
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'apikey': _apikey,
+      'language': _language,
+    });
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    // print(decodedData['results']);
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+    // print(peliculas.items[1].title);
+    return peliculas.items;
+  }
+}
